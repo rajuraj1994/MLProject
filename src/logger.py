0280fc81bcedc2cac8f04442f52c7f019228b18c
rajuraj1994 <rajurajkandel@gmail.com>
2026-01-20 13:42:54 +0545

    setup and logger

---
+++ src/logger.py
@@ -0,0 +1,18 @@
+import logging
+import os 
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+log_paths=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(log_paths,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(log_paths,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+# if __name__=="__main__":
+#     logging.info('Logging has started')
